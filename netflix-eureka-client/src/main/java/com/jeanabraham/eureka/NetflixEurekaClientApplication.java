8948cd91bc35ea2330fe51934be8766507117d34
jeanabraham <jean.abraham.is@gmail.com>
2017-07-20 10:56:12 -0400

    updating all clients to get data for config server

--- netflix-eureka-client/src/main/java/com/jeanabraham/eureka/NetflixEurekaClientApplication.java
+++ netflix-eureka-client/src/main/java/com/jeanabraham/eureka/NetflixEurekaClientApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,6 +25,7 @@ public class NetflixEurekaClientApplication {
 }
 
 
+@RefreshScope
 @RestController
 class ServiceInstanceRestController {
 
